dd92e6cbf3fb92624f42bbc268deeb83a1360351
rzemlin <34727847+rzemlin@users.noreply.github.com>
2021-06-24 08:21:13 -0700

    commit tree f6adaea17aaa1c50ab040872ba2aba67cd593292

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a= a
+    @b= b
+    @c= c
+  end
+
+  def kind
+    triangle_type
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+    def triangle_type
+      valid_triangle = [(a + b > c), (b + c > a), (a + c > b)]
+      [a, b, c].each do |side| valid_triangle << false if side <= 0 
+      raise TriangleError if valid_triangle.include?(false)
+      end
+    end
+
+    class TriangleError < StandardError
+    end
 end
